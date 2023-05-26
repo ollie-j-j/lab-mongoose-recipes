8592a0f9593d53857378a4fadd0f1d6a5951749e
Oliver Juggins <oliver.juggins@gmail.com>
2023-05-26 16:35:55 +0200

    lab complete

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const testRecipe = {
+  title: "Eggs on Toast",
+  level: "Amateur Chef",
+  ingredients: ["Bread", "Eggs", "Avocado"],
+  cuisine: "English",
+  dishtype: "breakfast",
+  duration: 10,
+  creator: "Joe Blogs"
+};
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -17,7 +27,38 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create(testRecipe);
   })
+  .then((recipe)=> console.log(`Title: ${recipe.title}`)) 
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then((data) => {
+    data.forEach((recipe) => {
+        console.log(`Title: ${recipe.title}`);
+    });
+  })
+  .then(() => {
+    return Recipe.findOne({ title: 'Rigatoni alla Genovese' })
+      .then(recipe => {
+        recipe.duration = 100;
+        return recipe.save();
+      })
+  })
+  .then(() => console.log("Updated the Rigatoni recipe duration to 100"))
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+      .then((Recipe) => console.log("Carrot Cake has been removed"))
+  })
+  .then(()=>  {
+    return mongoose.connection.close()
+  })
+  .then(()=> console.log('The connection is now closed'))
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+  
